4c7c0f135b5eb66d576221c8b279f9cd4bae555d
ike <ike@puppetlabs.vm>
2013-09-11 15:00:31 +0000

    added apache support for debian

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,26 +1,47 @@
 class apache {
+  case $::osfamily {
+    'debian': {
+      $httpd_user = 'www-data'
+      $httpd_group = 'www-data'
+      $httpd_pkg = 'apache2'
+      $httpd_svc = 'apache2'
+      $httpd_conf = '/etc/apache2/apache2.conf'
+    }
+    'redhat': {
+      $httpd_user = 'apache'
+      $httpd_group = 'apache'
+      $httpd_pkg = 'httpd'
+      $httpd_svc = 'httpd'
+      $httpd_conf = '/etc/httpd/conf/httpd.conf'
+    }
+    default:
+    {
+      fail("Module ${module_name} is not supported on ${::osfamily}")
+    }
+  }
+
   package { 'httpd':
     ensure => installed,
-    name   => 'httpd'
+    name   => $httpd_pkg
   }
 
   service { 'httpd':
     require => 'Package[httpd]',
     ensure  => running,
-    name    => 'httpd'
+    name    => $httpd_pkg
   }
 
   File {
     require => 'Package[httpd]', #adds the apache user and group
-    owner   => 'apache',
-    group   => 'apache',
+    owner   => $httpd_user,
+    group   => $httpd_group,
     mode    => '0644'
   }
 
-  file { 'httpd.conf':
+  file { 'config_file':
     ensure => file,
-    path   => '/etc/httpd/conf/httpd.conf',
-    source => 'puppet:///modules/apache/httpd.conf',
+    path   => $httpd_conf,
+    source => "puppet:///modules/apache/${httpd_pkg}.conf",
     notify => 'Service[httpd]'
   }
 
